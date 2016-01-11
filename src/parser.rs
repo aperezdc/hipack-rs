c6ed447a2a8aabb302e3c844af613415e0552297
Adrián Pérez de Castro <aperez@igalia.com>
2016-01-11 17:13:23 +0200

    Re-licensed as dual MIT/Apache-2.0
    See issue #1 for details.

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,10 @@
+// Copyright 2015-2016 hipack-rs developers
 //
-// parser.rs
-// Copyright (C) 2015 Adrian Perez <aperez@igalia.com>
-// Distributed under terms of the MIT license.
-//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
 
 use std::io;
 use std::fmt;
